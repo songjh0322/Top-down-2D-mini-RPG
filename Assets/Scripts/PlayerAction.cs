cd7be5c435addbe6d3a082861b2f6f1d649f1b30
Songjh0322 <happymine0322@naver.com>
2024-10-08 18:45:08 +0900

    Ray 쏘아주기

--- Assets/Scripts/PlayerAction.cs
+++ Assets/Scripts/PlayerAction.cs
@@ -10,6 +10,7 @@ public class PlayerAction : MonoBehaviour
     bool isHorizonMove; //수평이동인지를 확인하는 플래그
     Vector2 moveVec; //대각선 이동을 막기위한 벡터 변수
     public float speed = 5f;
+    Vector2 dirVec; //Ray를 쏘기위한 변수
     Rigidbody2D rigid;
     Animator anim;
 
@@ -24,6 +25,8 @@ public class PlayerAction : MonoBehaviour
         h = Input.GetAxisRaw("Horizontal");
         v = Input.GetAxisRaw("Vertical");
 
+
+
         if (h != 0 && v == 0) //수평이동할때
         {
             isHorizonMove = true;
@@ -33,17 +36,31 @@ public class PlayerAction : MonoBehaviour
             isHorizonMove = false;
         }
         // 애니메이션 상태 변경 감지 및 업데이트
-        if (anim.GetInteger("hAxisRaw") != (int)h || anim.GetInteger("vAxisRaw") != (int)v)
+        if (anim.GetInteger("hAxisRaw") != h)
         {
             anim.SetBool("isChange", true);
             anim.SetInteger("hAxisRaw", (int)h);
+        }
+        else if (anim.GetInteger("vAxisRaw") != v)
+        {
+            anim.SetBool("isChange", true);
             anim.SetInteger("vAxisRaw", (int)v);
         }
         else
         {
             anim.SetBool("isChange", false);
         }
-        
+
+        //Direction
+        if (v == 1)
+        { dirVec = Vector2.up; }
+        else if (v == -1)
+        { dirVec = Vector2.down; }
+        else if (h == 1)
+        { dirVec = Vector2.right; }
+        else if (h == -1)
+        { dirVec = Vector2.left; }
+
 
     }
     private void FixedUpdate()
@@ -57,6 +74,9 @@ public class PlayerAction : MonoBehaviour
             moveVec = new Vector2(0, v);
         }
         rigid.velocity = moveVec * speed;
+
+        //Ray
+        Debug.DrawRay(rigid.position, dirVec * 0.7f, new Color(0, 1, 0));
     }
 
     //Soil 타일맵에 들어가면 이동속도가 느려지는 함수
